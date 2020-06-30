9f1268119456bab1581a71ec35c5bf7f32cf3298
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2020-06-30 14:19:35 -0400

    simplify case of component testing -- in progress

---
+++ src/components/simpleComponent.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function SimpleComponent(props) {
+  return <div>Hello</div>
+}
+
+export default SimpleComponent
---
+++ src/components/simpleComponent.test.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { render } from "../utils/testUtils"
+
+import SimpleComponent from "./simpleComponent"
+
+test("Simple component renders", () => {
+  const simple = render(<SimpleComponent />)
+  console.log("simple component")
+  console.log(simple)
+})
