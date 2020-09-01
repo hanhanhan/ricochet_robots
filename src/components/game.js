473ebec60ea0c910516b42df6f650a0013010553
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2020-09-01 16:30:47 -0400

    use multi backend to support touch

--- src/components/game.js
+++ src/components/game.js
@@ -1,33 +1,34 @@
 import React from "react"
-import { DndProvider } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
+
+import { DndProvider } from "react-dnd-multi-backend"
+import HTML5ToTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch"
 import styled, { ThemeProvider } from "styled-components"
 
 import { GamePlayProvider } from "./providers"
-import { graphic } from "./themes"
+import { graphic } from "../css.themes"
 import Board from "./board"
 import Panel from "./panel"
-// Required ? deleting this makes dnd not work -- no errors -- why?
-import gamepieces from "../gameLogic/gamepieces"
-import { func } from "prop-types"
-import { turn } from "core-js/fn/dict"
 
 const Layout = styled.div`
   display: flex;
+  flex-direction: column;
+  @media (min-width: 900px) {
+    flex-direction: row;
+  }
 `
 
 function Game(props) {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <ThemeProvider theme={graphic}>
-        <GamePlayProvider>
-          <Layout>
-            <Panel />
+    <ThemeProvider theme={graphic}>
+      <GamePlayProvider>
+        <Layout>
+          <DndProvider options={HTML5ToTouch}>
             <Board />
-          </Layout>
-        </GamePlayProvider>
-      </ThemeProvider>
-    </DndProvider>
+            <Panel />
+          </DndProvider>
+        </Layout>
+      </GamePlayProvider>
+    </ThemeProvider>
   )
 }
 
